5ba05aa2daa6849ccb2810c93349ea28b34354a2
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-02-02 07:02:46 -0800

    setting the express app

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+require('dotenv').config()
+// express app
+const app = express();
+
+
+
+app.get('/', (req, res) => {
+    res.json({ mssg: 'server is live' })
+})
+// listen for requests
+app.listen(process.env.PORT, () => {
+    console.log("listening on port",process.env.PORT)
+})
+
