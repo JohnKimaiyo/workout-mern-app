98858a11c51ff74657b8b5f0d25cbaf2703a1bd6
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-02-03 02:55:31 -0800

    setting up mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const workoutRoutes = require("./routes/workouts");
 require("dotenv").config();
+const port = 4000
+
 // express app
 const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 // middleware
 app.use(express.json());
